cf91c363569a2d13032af00f0095cf52cd099c03
zhangzheng <1562124117@qq.com>
2023-07-06 20:57:40 +0800

    feat: 下载对应模板

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ program.version('1.0.0', '-v, --version')
     .action((templateName, projectName) => {
         if (templateName === "vue") {
             console.log('clone template ...',templateName,projectName);
-            // download('github:junkaicool/jkc-cli-vue-src', projectName, function (err) {
-            //     console.log(err ? 'Error' : 'Success')
-            // })
+            //对应的模板地址地址
+            download('github:qdfudimo/vue3-screen', projectName, function (err) {
+                console.log(err ? 'Error' : 'Success')
+            })
         } else if(templateName === "react") {
             console.log('clone template ...',templateName,projectName);
             // download('github:junkaicool/jkc-cli-react-src', projectName, function (err) {
